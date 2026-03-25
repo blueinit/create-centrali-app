7da6587aa88ecc185d527da2758986cf84be3ef5
Mary Olowu <meolowu@gmail.com>
2026-03-25 11:41:02 -0500

    Add Centrali image domains to Next.js config

--- templates/nextjs/next.config.ts
+++ templates/nextjs/next.config.ts
@@ -1,5 +1,30 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "centrali.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.centrali.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "centrali.dev",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.centrali.dev",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
